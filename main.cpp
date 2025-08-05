859efa5e0548ce1d964a91254b43ff82dc3a5f11
sdjuraev <s.djuraev@newuu.uz>
2025-08-05 10:38:28 +0500

    V1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Macbook M1 Pro 16 on 8/5/25.
+//
+
+#include "main.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "Hello World!" << endl;
+}
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by Macbook M1 Pro 16 on 8/5/25.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
